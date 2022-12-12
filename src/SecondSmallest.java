901a4ca5d68fa1183ccfef9419d05e307ff14029
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-12 14:42:57 +0530

    Second Smallest Element in array using sorting

---
+++ src/SecondSmallest.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SecondSmallest {
+
+    static void secondSmallest(int[] arr){
+        for(int i=0;i< arr.length;i++){
+            for(int j=i+1;j< arr.length;j++){
+                if(arr[i] > arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("Array sorted in Ascending order: "+ Arrays.toString(arr));
+
+        System.out.println("Second Smallest Element in Array: "+arr[1]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr={23,34,12,55,24};
+
+        secondSmallest(arr);
+    }
+}
