957dfcff6ca45b7b71e6dac1622fe46be25c6c5d
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-12 14:10:22 +0530

    Max & Min value

---
+++ src/MinMaxArray.java
@@ -0,0 +1,31 @@
+public class MinMaxArray {
+
+    static int maxArray(int[] arr){
+       int max=arr[0];
+       for (int i=1;i<arr.length;i++){
+           if(arr[i]>max){
+               max=arr[i];
+           }
+       }
+       return max;
+    }
+
+    static int minArray(int[] arr){
+        int min=arr[0];
+        for (int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12,4,43,77,34};
+        int a=maxArray(arr);
+        System.out.println("Maximum element of array: "+a);
+
+        int b=minArray(arr);
+        System.out.println("Mininum element of array: "+b);
+    }
+}
